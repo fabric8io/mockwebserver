50068a0d45d1477f8be89c8163f8b7bd47cc808d
Jorge Sartori <jorge.rocha@serpro.gov.br>
2019-08-28 19:20:20 -0300

    kubernetes-client_iss_1688 - Added type(with or without) in Attribute to allow without filter in extractor (key != value).

--- src/main/java/io/fabric8/mockwebserver/crud/Attribute.java
+++ src/main/java/io/fabric8/mockwebserver/crud/Attribute.java
@@ -1,19 +1,32 @@
 package io.fabric8.mockwebserver.crud;
 
+import static io.fabric8.mockwebserver.crud.AttributeType.WITH;
+
 public class Attribute {
 
     private final Key key;
     private final Value value;
+    private final AttributeType type;
 
 
-    public Attribute(String key, String value) {
-        this(new Key(key), new Value(value));
+    public Attribute(Key key, Value value, AttributeType type) {
+    	this.key = key;
+    	this.value = value;
+    	this.type = type;
+    }
+    
+    public Attribute(String key, String value, AttributeType type) {
+    	this(new Key(key), new Value(value), type);
     }
 
     public Attribute(Key key, Value value) {
-        this.key = key;
-        this.value = value;
+        this(key,value,WITH);
     }
+
+    public Attribute(String key, String value) {
+    	this(new Key(key), new Value(value));
+    }
+    
 
     public Key getKey() {
         return key;
@@ -48,4 +61,8 @@ public class Attribute {
                 ", value:" + value +
                 '}';
     }
+
+	public AttributeType getType() {
+		return type;
+	}
 }
---
+++ src/main/java/io/fabric8/mockwebserver/crud/AttributeType.java
@@ -0,0 +1,6 @@
+package io.fabric8.mockwebserver.crud;
+
+public enum AttributeType {
+	WITH,WITHOUT
+
+}
