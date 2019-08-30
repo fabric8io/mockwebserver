9d8105d683fd023c59eca295a34a2135d20e9c08
Jorge Sartori <jorge.rocha@serpro.gov.br>
2019-08-30 15:23:34 -0300

    kubernetes-client_iss_1688 - PR Refactor suggestions

--- src/main/java/io/fabric8/mockwebserver/crud/AttributeSet.java
+++ src/main/java/io/fabric8/mockwebserver/crud/AttributeSet.java
@@ -9,107 +9,94 @@ import java.util.Map;
 
 public class AttributeSet {
 
-	private final Map<Key, Attribute> attributes;
+    private final Map<Key, Attribute> attributes;
 
-	public static AttributeSet merge(AttributeSet... attributeSets) {
-		Map<Key, Attribute> all = new HashMap<>();
-		if (attributeSets != null) {
-			for (AttributeSet f : attributeSets) {
-				if (f != null && f.attributes != null) {
-					all.putAll(f.attributes);
-				}
-			}
-		}
-		return new AttributeSet(all);
-	}
+    public static AttributeSet merge(AttributeSet... attributeSets) {
+        Map<Key, Attribute> all = new HashMap<>();
+        if (attributeSets != null) {
+            for (AttributeSet f : attributeSets) {
+                if (f != null && f.attributes != null) {
+                    all.putAll(f.attributes);
+                }
+            }
+        }
+        return new AttributeSet(all);
+    }
 
-	public static AttributeSet map(Attribute... attributes) {
-		Map<Key, Attribute> all = new HashMap<>();
-		if (attributes != null) {
-			for (Attribute a : attributes) {
-				all.put(a.getKey(), a);
-			}
-		}
-		return new AttributeSet(all);
-	}
+    public static AttributeSet map(Attribute... attributes) {
+        Map<Key, Attribute> all = new HashMap<>();
+        if (attributes != null) {
+            for (Attribute a : attributes) {
+                all.put(a.getKey(), a);
+            }
+        }
+        return new AttributeSet(all);
+    }
 
-	public AttributeSet(Attribute... attributes) {
-		this(Arrays.asList(attributes));
-	}
+    public AttributeSet(Attribute... attributes) {
+        this(Arrays.asList(attributes));
+    }
 
-	public AttributeSet(Collection<Attribute> attributes) {
-		this(AttributeSet.map(attributes.toArray(new Attribute[attributes.size()])).attributes);
-	}
+    public AttributeSet(Collection<Attribute> attributes) {
+        this(AttributeSet.map(attributes.toArray(new Attribute[attributes.size()])).attributes);
+    }
 
-	public AttributeSet(Map<Key, Attribute> attributes) {
-		this.attributes = attributes;
-	}
+    public AttributeSet(Map<Key, Attribute> attributes) {
+        this.attributes = attributes;
+    }
 
-	public AttributeSet add(Attribute... attr) {
-		Map<Key, Attribute> all = new HashMap(attributes);
-		for (Attribute a : attr) {
-			all.put(a.getKey(), a);
-		}
-		return new AttributeSet(all);
-	}
+    public AttributeSet add(Attribute... attr) {
+        Map<Key, Attribute> all = new HashMap(attributes);
+        for(Attribute a : attr) {
+            all.put(a.getKey(), a);
+        }
+        return new AttributeSet(all);
+    }
 
-	public boolean containsKey(String key) {
-		return containsKey(new Key(key));
-	}
+    public boolean containsKey(String key) {
+        return containsKey(new Key(key));
+    }
 
-	public boolean containsKey(Key key) {
-		return attributes.containsKey(key);
-	}
+    public boolean containsKey(Key key) {
+        return attributes.containsKey(key);
+    }
+    
+    /**
+     * matches if attributes in db has (or doesn't if WITHOUT command) a set of candidate attributes
+     * @param candidate - set of candidate attributes
+     * @return match
+     */  
+    public boolean matches(AttributeSet candidate) {
+    	for (Attribute c : candidate.attributes.values()) {
+    		if (c.getType().equals(WITHOUT)) {
+    			return !attributes.values().contains(c);
+    		}
+    		if (!attributes.values().contains(c)) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
 
-	public boolean matches(AttributeSet candidate) {
-		for (Attribute c : candidate.attributes.values()) {
-			if (c.getType().equals(WITHOUT)) {
-				return without(c);
-			}
-			boolean found = false;
-			for (Attribute a : attributes.values()) {
-				if (c.equals(a)) {
-					found = true;
-				}
-			}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-			if (!found) {
-				return false;
-			}
-		}
-		return true;
-	}
+        AttributeSet that = (AttributeSet) o;
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
+        return attributes != null ? attributes.equals(that.attributes) : that.attributes == null;
+    }
 
-		AttributeSet that = (AttributeSet) o;
+    @Override
+    public int hashCode() {
+        return attributes != null ? attributes.hashCode() : 0;
+    }
 
-		return attributes != null ? attributes.equals(that.attributes) : that.attributes == null;
-	}
-
-	@Override
-	public int hashCode() {
-		return attributes != null ? attributes.hashCode() : 0;
-	}
-
-	@Override
-	public String toString() {
-		return "{" + "attributes: " + attributes + '}';
-	}
-
-	private boolean without(Attribute attribute) {
-//    	Key key = new Key(attribute.getKey().toString().replace("without_", ""));
-//    	Attribute c = new Attribute(key, attribute.getValue());
-		for (Attribute a : attributes.values()) {
-			if (attribute.equals(a)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    @Override
+    public String toString() {
+        return "{" +
+                "attributes: " + attributes  +
+                '}';
+    }
 }
